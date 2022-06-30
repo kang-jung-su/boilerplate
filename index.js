49812b127b3fe627bd12fda4034726f741ce7688
jskang <wjdtnrkd0203@naver.com>
2022-06-30 21:41:45 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,19 @@
+//백엔드 시작점
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://jskang:jskang@sokirok.7ogby.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! 안녕하세요");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
