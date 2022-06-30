0cbd9abb2d67861b2e8612c90316e55f67a32f26
jskang <wjdtnrkd0203@naver.com>
2022-06-30 23:04:33 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+//10자리 salt를 만들어서 암호화한다.
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +32,23 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
