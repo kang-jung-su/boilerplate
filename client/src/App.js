c90f3e34d605706ba7931356f8cb35c69a9ca256
kang-jung-su <wjdtnrkd0203@naver.com>
2022-07-07 14:50:59 +0900

    boilerplate complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LogingPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth";
 
 function App() {
+  const AuthLandinPage = Auth(LandingPage, null);
+  const AuthLoginPage = Auth(LogingPage, false);
+  const AuthRegisterPage = Auth(RegisterPage, false);
+
+
+
+
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/login" element={<LogingPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/" element={<AuthLandinPage />} />
+          <Route path="/login" element={<AuthLoginPage />} />
+          <Route path="/register" element={<AuthRegisterPage />} />
         </Routes>
       </div>
     </Router>
